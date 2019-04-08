827f2b49fa79c066082655e85055b9573761d059
keenmaster486 <cebrock106@gmail.com>
2019-04-08 16:47:59 -0600

    More methods and some test level data

--- js/app.js
+++ js/app.js
@@ -13,7 +13,8 @@ console.log(ctx); // cool, our rendering context is set up
 //Apply scaling factor
 const scaleX = 2.5;
 const scaleY = 3;
-applyScalingFactor();
+//applyScalingFactor();
+ctx.imageSmoothingEnabled = false;
 
 //Done setting up canvas!
 
@@ -24,8 +25,8 @@ const framerate = 30;
 //These are relative to the top left corner of the canvas (0, 0)
 //These must be added to any relative coordinates for drawing
 //For example, if sx = -50, everything will be drawn 50 pixels further to the left
-const sx = 0;
-const sy = 0;
+let sx = 0;
+let sy = 0;
 
 //Keyhandler var:
 const keys = [];
@@ -41,6 +42,16 @@ class Player
 		//x and y starting position:
 		this.x = x;
 		this.y = y;
+		this.width = 24;
+		this.height = 32;
+
+		//Physics vars:
+		this.gravity = 1;
+		this.termvel = 6;
+		this.walkspeed = 4;
+		this.jumpspeed = -12;
+		this.yv = 0;
+		this.ya = 0;
 
 		this.numsprites = ns;
 		this.frame = 0;
@@ -57,7 +68,7 @@ class Player
 			//Needed because otherwise getImageData complains about security issues:
 			imgid.setAttribute('crossOrigin', 'Anonymous');
 			console.log(imgid.height, imgid.width);
-			ctx.drawImage(imgid, 0, 0, imgid.width, imgid.height);
+			ctx.drawImage(imgid, 0, 0, imgid.width*scaleX, imgid.height*scaleY);
 
 			//Handle transparent color:
 			imgData = ctx.getImageData(0, 0, imgid.width*scaleX, imgid.height*scaleY);
@@ -74,10 +85,10 @@ class Player
 			}
 			console.log(`${this.name}: Replaced transparent color index with alpha=0`);
 			//Put image back on screen with transparent color enabled:
-			ctx.putImageData(imgData, 0, 0);
+			//ctx.putImageData(imgData, 0, 0);
 
 
-			imgData = ctx.getImageData(0, 0, imgid.width*scaleX, imgid.height*scaleY);
+			//imgData = ctx.getImageData(0, 0, imgid.width*scaleX, imgid.height*scaleY);
 			clearCanvas();
 			this.sprites[i] = imgData;
 		}
@@ -87,18 +98,29 @@ class Player
 	{
 		//s is the number of the sprite to draw (starting at 0)
 		//x, y is the position to draw at (in pixels, relative, so we must add sx, sy)
-		ctx.putImageData(this.sprites[s], (x*scaleX) + sx, (y*scaleY) + sy);
+		
+		//Create an intermediary canvas to draw our sprite on so transparency works:
+		const overlayCanvas = document.createElement("canvas");
+		overlayCanvas.width = this.width*scaleX;
+		overlayCanvas.height = this.height*scaleY;
+		overlayCanvas.getContext("2d").putImageData(this.sprites[s], 0, 0);
+		ctx.drawImage(overlayCanvas, (x + sx)*scaleX, (y + sy)*scaleY);
+		//ctx.putImageData(this.sprites[s], (x*scaleX) + sx, (y*scaleY) + sy);
 	}
 
 	handleInput()
 	{
 		if (keys["ArrowLeft"])
 		{
-			this.x = this.x - 2;
+			this.x = this.x - this.walkspeed;
 		}
 		if (keys["ArrowRight"])
 		{
-			this.x = this.x + 2;
+			this.x = this.x + this.walkspeed;
+		}
+		if (keys["Control"] && this.allowjump)
+		{
+			this.jump();
 		}
 	}
 
@@ -106,8 +128,32 @@ class Player
 	{
 		this.drawSprite(0, this.x, this.y);
 	}
+
+	increment()
+	{
+		this.ya = this.gravity;
+		this.yv = this.yv + this.ya;
+		if (this.yv > this.termvel) {this.yv = this.termvel;}
+		this.y = this.y + this.yv;
+	}
+
+	jump()
+	{
+		this.yv = this.jumpspeed;
+		this.increment();
+		this.allowjump = false;
+		console.log("JUMP");
+	}
 }
 
+class Tile
+{
+	constructor(d, t)
+	{
+		this.data = d;
+		this.type = t;
+	}
+}
 
 class Tiles
 {
@@ -126,28 +172,22 @@ class Tiles
 		let imgData = ctx.getImageData(0, 0, 1, 1); //create an empty imgData object
 		let imgid = document.getElementById("til-img") //for now
 		imgid.setAttribute('crossOrigin', 'Anonymous');
-		ctx.drawImage(imgid, 0, 0, imgid.width, imgid.height);
 		
-		//Handle transparent color:
-		imgData = ctx.getImageData(0, 0, 320*scaleX, 200*scaleY)
-		for (let j = 0; j < imgData.data.length; j+=4)
-		{
-			//console.log(imgData[j], imgData[j + 1], imgData[j + 2]);
-			//console.log("test");
-			if (imgData.data[j] == 192 && imgData.data[j + 1] == 220 && imgData.data[j + 2] == 192)
-			{
-				//We've found a transparent color so set alpha channel to max:
-				imgData.data[j + 3] = 0;
-				//console.log("Transparent color found");
-			}
-		}
-		console.log(`${this.name}: Replaced transparent color index with alpha=0`);
+		ctx.drawImage(imgid, 0, 0, imgid.width*scaleX, imgid.height*scaleY);
+		
+
+		//console.log(`${this.name}: Replaced transparent color index with alpha=0`);
 		//Put image back on screen with transparent color enabled:
-		ctx.putImageData(imgData, 0, 0);
+		//ctx.putImageData(imgData, 0, 0);
 
 		//these will be the coordinates of the section of the til-img to copy from
 		let tempx = 0;
 		let tempy = 0;
+		let numtr = 0; //counter for number of transparent tiles replaced
+		// let blanktile = {
+		// 	data: undefined,
+		// 	type: undefined,
+		// };
 		this.tiles = [];
 		for (let i = 0; i < 240; i++)
 		{
@@ -156,21 +196,206 @@ class Tiles
 			//console.log(i, tempx, tempy);
 			imgData = ctx.getImageData(tempx*scaleX, tempy*scaleY, 16*scaleX, 16*scaleY);
 
+			//Handle transparent color:
+			//imgData = ctx.getImageData(0, 0, 320*scaleX, 200*scaleY)
+			for (let j = 0; j < imgData.data.length; j+=4)
+			{
+				//console.log(imgData[j], imgData[j + 1], imgData[j + 2]);
+				//console.log("test");
+				if (imgData.data[j] == 192 && imgData.data[j + 1] == 220 && imgData.data[j + 2] == 192)
+				{
+					//We've found a transparent color so set alpha channel to max:
+					//imgData.data[j] = 0;
+					//imgData.data[j + 1] = 0;
+					//imgData.data[j + 2] = 0;
+					imgData.data[j + 3] = 0;
+					//console.log("Transparent color found");
+					numtr++;
+				}
+			}
+			//blanktile.data = imgData;
+			//blanktile.type = 0;
+			this.tiles[i] = new Tile(imgData, 0);
+			if (i > 20 && i < 30) {this.tiles[i].type = 1;}
+			//this.tiles[i].data = imgData;
+			//this.tiles[i].type = 0;
+		}
 
-
-			this.tiles[i] = imgData;
+		if (numtr > 0)
+		{
+			console.log(`${this.name}: Replaced transparent color index with alpha=0 for ${numtr} pixels`);
 		}
 		clearCanvas();
 	}
 
 	drawTile(t, x, y)
 	{
-		//t is the tile to draw (an arbitrary integer)
+		//t is the tile to draw (an arbitrary integer, indexed from 0)
 		//x, y are the coordinates (relative, so must add sx, sy)
 		//This function must take the variable t and
 		//perform an operation on it to extract the coordinates
 		//in the tile image of the tile we're trying to get
-		ctx.putImageData(this.tiles[t], x*scaleX, y*scaleY);
+		
+		t = t - 1;
+		if (t < 0) {return false;}
+
+		//Create an intermediary canvas to draw our tile on so transparency works:
+		const overlayCanvas = document.createElement("canvas");
+		overlayCanvas.width = 16*scaleX;
+		overlayCanvas.height = 16*scaleY;
+		overlayCanvas.getContext("2d").putImageData(this.tiles[t].data, 0, 0);
+		ctx.drawImage(overlayCanvas, (x + sx)*scaleX, (y + sy)*scaleY);
+
+		return true;
+		//ctx.putImageData(this.tiles[t], x*scaleX, y*scaleY);
+	}
+}
+
+
+class Level
+{
+	constructor()
+	{
+		//level class
+		//contains the level map and draws the tiles
+
+		this.player1 = new Player("Player 1", 0, 0, 1);
+		this.backtiles = new Tiles("Background tiles");
+		
+		startKeyHandler();
+		
+		const itself = this;
+
+		setInterval(function()
+		{
+			itself.handleKeyEvents();
+			itself.drawAll();
+			itself.incrementAll();
+		}, 1000/framerate);
+
+		this.getLevelData();
+	}
+
+	checkCollisionWithTile(tile)
+	{
+		//tile is a tile object which contains the
+		//data of the tile we're checking, such as
+		//position, type, etc.
+
+		//obj is the object to check collision with
+		//could be a player or enemy class
+		//all it needs to have is x, y and physics vars to access
+
+		if ((this.player1.y > tile.y - this.player1.height) && (this.player1.x + this.player1.width > tile.x && this.player1.x < tile.x + 16) && tile.type == 1)
+		{
+			//console.log("collision!");
+			this.player1.y = tile.y - this.player1.height;
+			this.player1.yv = 0;
+			this.player1.ya = 0;
+			this.player1.allowjump = true;
+		}
+	}
+
+	getLevelData()
+	{
+		//Create some level data:
+		//(this is a test level that is two "screens" square (40x24 tiles))
+
+		this.data = [
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 1, 2, 3, 3, 3, 4, 3, 3, 3, 4, 3, 4, 3, 3, 4, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[21,22,23,22,23,22,23,22,23,22,23,22,23,22,23,23,24,25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[61,62,63,62,63,62,63,62,63,62,63,62,63,62,63,63,64,65, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		]
+	}
+
+	drawAll()
+	{
+		clearCanvas();
+
+		// //=======SOME TEST CODE=======
+		// this.backtiles.drawTile(0, 0, 144);
+		// this.backtiles.drawTile(1, 16, 144);
+		// this.backtiles.drawTile(2, 32, 144);
+		// this.backtiles.drawTile(3, 48, 144);
+		// this.backtiles.drawTile(4, 64, 144);
+
+		// this.backtiles.drawTile(20, 0, 160);
+		// this.backtiles.drawTile(21, 16, 160);
+		// this.backtiles.drawTile(22, 32, 160);
+		// this.backtiles.drawTile(23, 48, 160);
+		// this.backtiles.drawTile(24, 64, 160);
+
+		// this.backtiles.drawTile(60, 0, 176);
+		// this.backtiles.drawTile(61, 16, 176);
+		// this.backtiles.drawTile(62, 32, 176);
+		// this.backtiles.drawTile(63, 48, 176);
+		// this.backtiles.drawTile(64, 64, 176);
+		// //=============================
+		this.drawTiles();
+		this.player1.draw();
+	}
+
+	drawTiles()
+	{
+		//Figure out what section of tiles to draw:
+
+		//Draw the tiles:
+		for (let j = 0; j < 20; j++)
+		{
+			for (let k = 0; k < 12; k++)
+			{
+				this.backtiles.drawTile(this.data[k][j], j*16, k*16);
+				if (this.data[k][j] > 0)
+				{
+					this.checkCollisionWithTile({x: j*16, y: k*16, type: this.backtiles.tiles[this.data[k][j]].type});
+				}
+			}
+		}
+	}
+
+	incrementAll()
+	{
+		this.player1.increment();
+		this.handleScrolling();
+	}
+
+	handleScrolling()
+	{
+		if ((this.player1.x+sx) > 150)
+		{
+			sx = sx - this.player1.walkspeed;
+		}
+		if ((this.player1.x+sx) < 50)
+		{
+			sx = sx + this.player1.walkspeed;
+		}
+		if (sx > 0) {sx = 0;}
+	}
+
+	handleKeyEvents()
+	{
+		this.player1.handleInput();
 	}
 }
 
@@ -191,34 +416,7 @@ function startKeyHandler()
 	});
 }
 
-function handleKeyEvents()
-{
-	player1.handleInput();
-}
 
-function drawAll()
-{
-	clearCanvas();
-	player1.draw();
-	backtiles.drawTile(0, 0, 144);
-	backtiles.drawTile(1, 16, 144);
-	backtiles.drawTile(2, 32, 144);
-	backtiles.drawTile(3, 48, 144);
-	backtiles.drawTile(4, 64, 144);
-
-	backtiles.drawTile(20, 0, 160);
-	backtiles.drawTile(21, 16, 160);
-	backtiles.drawTile(22, 32, 160);
-	backtiles.drawTile(23, 48, 160);
-	backtiles.drawTile(24, 64, 160);
-
-	backtiles.drawTile(60, 0, 176);
-	backtiles.drawTile(61, 16, 176);
-	backtiles.drawTile(62, 32, 176);
-	backtiles.drawTile(63, 48, 176);
-	backtiles.drawTile(64, 64, 176);
-
-}
 
 function applyScalingFactor()
 {
@@ -236,24 +434,10 @@ function isLoaded()
   return document.readyState == "complete";
 }
 
-setInterval(function()
-{
-	handleKeyEvents();
-	drawAll();
-}, 1000/framerate);
-
-
-//while (!isLoaded()) {}
-
-const player1 = new Player("Player 1", 0, 0, 1);
-const backtiles = new Tiles("Background tiles");
-//applyScalingFactor();
-startKeyHandler();
-//applyScalingFactor();
-//ctx.scale(scaleX, scaleY);
-//ctx.imageSmoothingEnabled = false;
-//player1.drawSprite(0, 50, 50);
 
 
 
 
+
+
+const testlevel = new Level();
